10d6c896b60bc989b378b4265ebecdd83ec0a025
Alaa <alaabinothman93@gmail.com>
2023-05-30 21:53:26 +0300

    add update and delete

--- BlogWebsite/main_app/urls.py
+++ BlogWebsite/main_app/urls.py
@@ -5,5 +5,9 @@ app_name = "main_app"
 
 urlpatterns = [
     path("", views.index_page, name="index_page"),
-    path("posts/add/", views.add_post, name="add_post")
+    path("posts/add/", views.add_post, name="add_post"),
+    path("posts/details/<post_id>/", views.post_detail, name="post_detail"),
+    path("posts/update/<post_id>/", views.update_post, name="update_post"),
+    path("games/delete/<post_id>/", views.delete_post, name="delete_post"),
+    path("games/search/", views.search_page, name="search_page"),
     ]
--- BlogWebsite/main_app/views.py
+++ BlogWebsite/main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpRequest, HttpResponse, HttpResponseNotFound,Http404
 from .models import Post
 
 # Create your views here.
@@ -22,3 +22,50 @@ def index_page(request:HttpRequest):
     posts = Post.objects.all()
 
     return render(request, "main_app/index.html", {"posts" : posts})
+
+
+def post_detail(request:HttpRequest, post_id):
+
+    try:
+        post = Post.objects.get(id=post_id)
+    except Post.DoesNotExist:
+        raise Http404("Post does not exist")
+    return render(request, 'main_app/post_detail.html', {"post" : post})
+
+
+def update_post(request:HttpRequest, post_id):
+
+    post = Post.objects.get(id=post_id)
+    iso_date = post.publish_date.isoformat().replace("T", " ").split("+")[0]
+    print(iso_date)
+    """ - Title : char field
+    - Content : text field
+    - is_published : boolean field
+    - publish_date : datetime field """
+    #updating the post
+    if request.method == "POST":
+        post.title = request.POST["title"]
+        post.content = request.POST["content"]
+        post.is_published = request.POST["is_published"]
+        post.publish_date = request.POST["publish_date"]
+        post.save()
+
+        return redirect("main_app:post_detail", post_id=post.id)
+
+    return render(request, 'main_app/update_post.html', {"post" : post, "iso_date" : iso_date})
+
+
+
+def delete_post(request:HttpRequest, post_id):
+    
+    post = Post.objects.get(id=post_id)
+    post.delete()
+
+    return redirect("main_app:index_page")
+
+
+def search_page(request:HttpRequest):
+    search_phrase = request.GET.get("search", "")
+    posts = Post.objects.filter(title__contains=search_phrase, is_published=True)
+
+    return render(request, "main_app/search_page.html", {"posts" : posts})
